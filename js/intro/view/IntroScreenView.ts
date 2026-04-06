d64a010d53ad669ba36ad37793e86c0e123f83a0
Jesse <jesse.greenberg@colorado.edu>
2026-04-06 16:18:40 -0400

    add a prototype number control for changing work function, see #5

--- js/intro/view/IntroScreenView.ts
+++ js/intro/view/IntroScreenView.ts
@@ -6,13 +6,19 @@
  * @author Marla A. Schulz
  */
 
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import DynamicProperty from '../../../../axon/js/DynamicProperty.js';
 import ScreenView, { ScreenViewOptions } from '../../../../joist/js/ScreenView.js';
+import Range from '../../../../dot/js/Range.js';
 import optionize from '../../../../phet-core/js/optionize.js';
 import ResetAllButton from '../../../../scenery-phet/js/buttons/ResetAllButton.js';
+import NumberControl from '../../../../scenery-phet/js/NumberControl.js';
+import Text from '../../../../scenery/js/nodes/Text.js';
 import ComboBox from '../../../../sun/js/ComboBox.js';
 import PhotoelectricEffectConstants from '../../common/PhotoelectricEffectConstants.js';
+import Material, { MaterialType } from '../../common/model/Material.js';
 import PhotoelectricEffectModel from '../../common/model/PhotoelectricEffectModel.js';
-import Text from '../../../../scenery/js/nodes/Text.js';
+import PhotoelectricEffectFluent from '../../PhotoelectricEffectFluent.js';
 
 type SelfOptions = {
  //TODO add options that are specific to PhotoelectricEffectScreenView here
@@ -33,7 +39,7 @@ export default class IntroScreenView extends ScreenView {
     const comboBoxItems = model.target.materials.map( ( material, i ) => {
       return {
         value: material,
-        createNode: () => new Text( `Material ${i}` )
+        createNode: () => new Text( `Material ${i}, ${material.materialType}` )
       };
     } );
 
@@ -42,6 +48,30 @@ export default class IntroScreenView extends ScreenView {
     } );
 
     this.addChild( materialsComboBox );
+
+    const workFunctionProperty = new DynamicProperty<number, number, Material>( model.target.materialProperty, {
+      bidirectional: true,
+      derive: 'workFunctionProperty'
+    } );
+    const customMaterialSelectedProperty = new DerivedProperty( [ model.target.materialProperty ],
+      material => material.materialType === MaterialType.CUSTOM );
+
+    const workFunctionControl = new NumberControl(
+      PhotoelectricEffectFluent.introScreen.workFunction.labelStringProperty,
+      workFunctionProperty,
+      new Range( 0, 10 ),
+      {
+        delta: 0.1,
+        numberDisplayOptions: {
+          decimalPlaces: 1
+        },
+        visibleProperty: customMaterialSelectedProperty,
+        tandem: options.tandem.createTandem( 'workFunctionControl' )
+      }
+    );
+    workFunctionControl.centerX = materialsComboBox.centerX;
+    workFunctionControl.top = materialsComboBox.bottom + 20;
+    this.addChild( workFunctionControl );
 
     const resetAllButton = new ResetAllButton( {
       listener: () => {
