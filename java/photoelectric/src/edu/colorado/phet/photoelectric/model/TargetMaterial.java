04af9a7bbe96866dfc45a0067fdbff1122597c6d
Jesse <jesse.greenberg@colorado.edu>
2026-04-06 14:43:33 -0400

    adding java code for context, temporarily

---
+++ java/photoelectric/src/edu/colorado/phet/photoelectric/model/TargetMaterial.java
@@ -0,0 +1,27 @@
+// Copyright 2002-2011, University of Colorado
+
+/*
+ * CVS Info -
+ * Filename : $Source$
+ * Branch : $Name$
+ * Modified by : $Author: samreid $
+ * Revision : $Revision: 47772 $
+ * Date modified : $Date: 2011-01-07 12:53:07 -0700 (Fri, 07 Jan 2011) $
+ */
+package edu.colorado.phet.photoelectric.model;
+
+import edu.colorado.phet.common.quantum.model.ElementProperties;
+
+/**
+ * TargetMaterial
+ *
+ * @author Ron LeMaster
+ * @version $Revision: 47772 $
+ */
+public class TargetMaterial {
+    private ElementProperties properties;
+
+    public TargetMaterial( ElementProperties properties ) {
+        this.properties = properties;
+    }
+}
