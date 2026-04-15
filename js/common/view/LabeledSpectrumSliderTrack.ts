597e504790e6e64f82dab123e2c60951e1ad0352
Jesse <jesse.greenberg@colorado.edu>
2026-04-15 18:17:52 -0400

    initial commit for intensity and wavelength control, see https://github.com/phetsims/photoelectric-effect/issues 25

--- js/common/view/LabeledSpectrumSliderTrack.ts
+++ js/common/view/LabeledSpectrumSliderTrack.ts
@@ -9,6 +9,7 @@
 import Multilink from '../../../../axon/js/Multilink.js';
 import type TProperty from '../../../../axon/js/TProperty.js';
 import Range from '../../../../dot/js/Range.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
 import SpectrumSliderTrack, { SpectrumSliderTrackOptions } from '../../../../scenery-phet/js/SpectrumSliderTrack.js';
 import VisibleColor from '../../../../scenery-phet/js/VisibleColor.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
@@ -22,14 +23,18 @@ export default class LabeledSpectrumSliderTrack extends SpectrumSliderTrack {
 
   public constructor( property: TProperty<number>, range: Range, providedOptions?: SpectrumSliderTrackOptions ) {
 
-    super( property, range, providedOptions );
+    super( property, range, combineOptions<SpectrumSliderTrackOptions>( {}, providedOptions, {
+      isDisposable: false
+    } ) );
 
     const uvText = new Text( PhotoelectricEffectFluent.spectrumTrack.uvLabelStringProperty, {
-      font: SPECTRUM_BAND_LABEL_FONT
+      font: SPECTRUM_BAND_LABEL_FONT,
+      maxWidth: 50
     } );
 
     const irText = new Text( PhotoelectricEffectFluent.spectrumTrack.irLabelStringProperty, {
-      font: SPECTRUM_BAND_LABEL_FONT
+      font: SPECTRUM_BAND_LABEL_FONT,
+      maxWidth: 50
     } );
 
     this.addChild( uvText );
