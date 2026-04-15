d2152e70511b766415de8f7b94ca2955f2fb1cf1
Jesse <jesse.greenberg@colorado.edu>
2026-04-15 18:17:52 -0400

    sync-to-poly: initial commit for intensity and wavelength control, see https://github.com/phetsims/photoelectric-effect/issues/25

---
+++ js/common/view/GradientBackplateIntensitySlider.ts
@@ -0,0 +1,155 @@
+// Copyright 2026, University of Colorado Boulder
+
+/**
+ * Horizontal intensity slider with a full-width gradient backplate (black to the current wavelength color)
+ * and a percent NumberDisplay to the right of the slider. Local bounds are overridden to the slider-plus-gradient
+ * region only so parent layout (e.g. VBox) does not treat the readout as part of this control's box. This makes
+ * centering relative to the gradient rectangle simple.
+ *
+ * @author Marla Schulz (PhET Interactive Simulations)
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ */
+
+import Multilink from '../../../../axon/js/Multilink.js';
+import Dimension2 from '../../../../dot/js/Dimension2.js';
+import Range from '../../../../dot/js/Range.js';
+import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import NumberDisplay, { NumberDisplayOptions } from '../../../../scenery-phet/js/NumberDisplay.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
+import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
+import Color from '../../../../scenery/js/util/Color.js';
+import LinearGradient from '../../../../scenery/js/util/LinearGradient.js';
+import HSlider from '../../../../sun/js/HSlider.js';
+import PhotoelectricEffectFluent from '../../PhotoelectricEffectFluent.js';
+import { wavelengthToIntensityGradientEndColor } from '../model/PhotoelectricEffectUtils.js';
+import PhotonSource from '../model/PhotonSource.js';
+
+type SelfOptions = {
+  trackSize?: Dimension2;
+  thumbSize?: Dimension2;
+  readoutMaxWidth?: number;
+};
+
+export type GradientBackplateIntensitySliderOptions = SelfOptions & PickRequired<NodeOptions, 'tandem'>;
+
+// Horizontal gap between the gradient rectangle and the percent readout.
+const READOUT_SPACING = 6;
+
+const READOUT_FONT = new PhetFont( 12 );
+
+const NUMBER_DISPLAY_BASE: NumberDisplayOptions = {
+  decimalPlaces: 0,
+  cornerRadius: 4,
+  backgroundFill: 'white',
+  backgroundStroke: 'black',
+  backgroundLineWidth: 1,
+  align: 'center',
+  xMargin: 2
+};
+
+export default class GradientBackplateIntensitySlider extends Node {
+
+  public constructor( photonSource: PhotonSource, providedOptions: GradientBackplateIntensitySliderOptions ) {
+
+    const options = optionize<GradientBackplateIntensitySliderOptions, SelfOptions, NodeOptions>()( {
+      trackSize: new Dimension2( 125, 5 ),
+      thumbSize: new Dimension2( 13, 26 ),
+      readoutMaxWidth: 72,
+      isDisposable: false
+    }, providedOptions );
+
+    super();
+
+    const intensityGradientRectangle = new Rectangle( 0, 0, 1, 1, {
+      stroke: 'black',
+      lineWidth: 1
+    } );
+
+    const intensitySlider = new HSlider( photonSource.intensityProperty, photonSource.intensityProperty.range, {
+      tandem: options.tandem,
+      trackSize: options.trackSize,
+      thumbSize: options.thumbSize
+    } );
+
+    const intensityReadout = new NumberDisplay( photonSource.intensityPercentProperty, new Range(
+      photonSource.intensityProperty.range.min * 100,
+      photonSource.intensityProperty.range.max * 100
+    ), combineOptions<NumberDisplayOptions>(
+      {},
+      NUMBER_DISPLAY_BASE,
+      {
+        valuePattern: PhotoelectricEffectFluent.intensity.percentReadoutPatternStringProperty,
+        textOptions: {
+          font: READOUT_FONT,
+          maxWidth: options.readoutMaxWidth
+        },
+        tandem: options.tandem.createTandem( 'intensityReadout' )
+      }
+    ) );
+
+    this.addChild( intensityGradientRectangle );
+    this.addChild( intensitySlider );
+    this.addChild( intensityReadout );
+
+    this.mutate( options );
+
+    Multilink.multilink(
+      [ photonSource.wavelengthProperty, intensitySlider.boundsProperty ],
+      () => {
+        GradientBackplateIntensitySlider.updateGradientAndReadoutLayout(
+          this,
+          photonSource,
+          intensityGradientRectangle,
+          intensitySlider,
+          intensityReadout
+        );
+      }
+    );
+    GradientBackplateIntensitySlider.updateGradientAndReadoutLayout(
+      this,
+      photonSource,
+      intensityGradientRectangle,
+      intensitySlider,
+      intensityReadout
+    );
+  }
+
+  /**
+   * Sizes and positions the gradient under the slider and aligns the percent readout.
+   * Updates localBounds to the slider and gradient only (readout excluded) for parent layout.
+   */
+  private static updateGradientAndReadoutLayout(
+    sliderContainer: GradientBackplateIntensitySlider,
+    photonSource: PhotonSource,
+    intensityGradientRectangle: Rectangle,
+    intensitySlider: HSlider,
+    intensityReadout: NumberDisplay
+  ): void {
+    const w = Math.max( intensitySlider.width, 1 );
+    const h = Math.max( intensitySlider.height, 1 );
+    intensityGradientRectangle.setRect( 0, 0, w, h );
+    intensityGradientRectangle.left = intensitySlider.left;
+    intensityGradientRectangle.top = intensitySlider.top;
+
+    const endColor = wavelengthToIntensityGradientEndColor( photonSource.wavelengthProperty.value );
+    intensityGradientRectangle.fill = new LinearGradient( 0, 0, w, 0 )
+      .addColorStop( 0, Color.BLACK )
+      .addColorStop( 1, endColor );
+
+    intensityReadout.left = intensityGradientRectangle.right + READOUT_SPACING;
+    intensityReadout.centerY = intensityGradientRectangle.centerY;
+
+    const sliderAndGradientBoundsLocal = intensitySlider.bounds.union( intensityGradientRectangle.bounds );
+
+    if ( sliderAndGradientBoundsLocal.isValid() ) {
+      sliderContainer.setLocalBounds( sliderAndGradientBoundsLocal );
+    }
+    else {
+
+      // Fall back to automatic bounds until the slider has valid geometry.
+      sliderContainer.setLocalBounds( null );
+    }
+  }
+}
---
+++ js/common/view/WavelengthSliderWithReadout.ts
@@ -0,0 +1,183 @@
+// Copyright 2026, University of Colorado Boulder
+
+/**
+ * Wavelength control: HSlider with labeled spectrum track, spectrum thumb, and a NumberDisplay (nm) positioned
+ * above the thumb and clamped to the track span.
+ *
+ * @author Marla Schulz (PhET Interactive Simulations)
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ */
+
+import Multilink from '../../../../axon/js/Multilink.js';
+import Dimension2 from '../../../../dot/js/Dimension2.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import { clamp } from '../../../../dot/js/util/clamp.js';
+import optionize, { combineOptions } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import NumberDisplay, { NumberDisplayOptions } from '../../../../scenery-phet/js/NumberDisplay.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import SpectrumSliderThumb from '../../../../scenery-phet/js/SpectrumSliderThumb.js';
+import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
+import HSlider from '../../../../sun/js/HSlider.js';
+import Slider from '../../../../sun/js/Slider.js';
+import PhotoelectricEffectFluent from '../../PhotoelectricEffectFluent.js';
+import { wavelengthToColor } from '../model/PhotoelectricEffectUtils.js';
+import PhotonSource from '../model/PhotonSource.js';
+import LabeledSpectrumSliderTrack from './LabeledSpectrumSliderTrack.js';
+
+type SelfOptions = {
+  trackSize?: Dimension2;
+  thumbWidth?: number;
+  thumbHeight?: number;
+  readoutMaxWidth?: number;
+  readoutAboveSliderSpacing?: number;
+};
+
+export type WavelengthSliderWithReadoutOptions = SelfOptions & PickRequired<NodeOptions, 'tandem'>;
+
+const DEFAULT_TRACK_SIZE = new Dimension2( 240, 20 );
+
+const READOUT_FONT = new PhetFont( 12 );
+
+const NUMBER_DISPLAY_BASE: NumberDisplayOptions = {
+  decimalPlaces: 0,
+  cornerRadius: 4,
+  backgroundFill: 'white',
+  backgroundStroke: 'black',
+  backgroundLineWidth: 1,
+  align: 'center',
+  xMargin: 2
+};
+
+export default class WavelengthSliderWithReadout extends Node {
+
+  public constructor( photonSource: PhotonSource, providedOptions: WavelengthSliderWithReadoutOptions ) {
+
+    const options = optionize<WavelengthSliderWithReadoutOptions, SelfOptions, NodeOptions>()( {
+      trackSize: DEFAULT_TRACK_SIZE,
+      thumbWidth: 18,
+      thumbHeight: 25,
+      readoutMaxWidth: 100,
+      readoutAboveSliderSpacing: 5,
+      isDisposable: false
+    }, providedOptions );
+
+    super();
+
+    const wavelengthThumbNode = new SpectrumSliderThumb( photonSource.wavelengthProperty, {
+      valueToColor: wavelengthToColor,
+      width: options.thumbWidth,
+      height: options.thumbHeight,
+      cursorHeight: options.trackSize.height,
+      tandem: options.tandem.createTandem( Slider.THUMB_NODE_TANDEM_NAME )
+    } );
+
+    const wavelengthTrackNode = new LabeledSpectrumSliderTrack( photonSource.wavelengthProperty, photonSource.wavelengthProperty.range, {
+      valueToColor: wavelengthToColor,
+      size: options.trackSize,
+      tandem: options.tandem.createTandem( Slider.TRACK_NODE_TANDEM_NAME )
+    } );
+
+    const wavelengthSlider = new HSlider( photonSource.wavelengthProperty, photonSource.wavelengthProperty.range, {
+      tandem: options.tandem,
+      trackNode: wavelengthTrackNode,
+      thumbNode: wavelengthThumbNode
+    } );
+
+    const wavelengthReadout = new NumberDisplay( photonSource.wavelengthProperty, photonSource.wavelengthProperty.range, combineOptions<NumberDisplayOptions>(
+      {},
+      NUMBER_DISPLAY_BASE,
+      {
+        valuePattern: PhotoelectricEffectFluent.wavelength.valueReadoutPatternStringProperty,
+        textOptions: {
+          font: READOUT_FONT,
+          maxWidth: options.readoutMaxWidth
+        },
+        tandem: options.tandem.createTandem( 'wavelengthReadout' )
+      }
+    ) );
+
+    this.addChild( wavelengthSlider );
+    this.addChild( wavelengthReadout );
+
+    this.mutate( options );
+
+    Multilink.multilink(
+      [
+        photonSource.wavelengthProperty,
+        photonSource.wavelengthProperty.rangeProperty,
+        this.boundsProperty,
+        wavelengthSlider.boundsProperty,
+        wavelengthThumbNode.boundsProperty,
+        wavelengthReadout.boundsProperty,
+        wavelengthTrackNode.boundsProperty,
+        wavelengthTrackNode.valueToPositionProperty
+      ],
+      () => {
+        WavelengthSliderWithReadout.updateReadoutLayout(
+          photonSource,
+          this,
+          wavelengthSlider,
+          wavelengthThumbNode,
+          wavelengthReadout,
+          wavelengthTrackNode,
+          options.readoutAboveSliderSpacing
+        );
+      }
+    );
+    WavelengthSliderWithReadout.updateReadoutLayout(
+      photonSource,
+      this,
+      wavelengthSlider,
+      wavelengthThumbNode,
+      wavelengthReadout,
+      wavelengthTrackNode,
+      options.readoutAboveSliderSpacing
+    );
+  }
+
+  /**
+   * Keeps the readout centered above the thumb while clamping its center to the visible track span.
+   */
+  private static updateReadoutLayout(
+    photonSource: PhotonSource,
+    rowNode: Node,
+    wavelengthSlider: HSlider,
+    wavelengthThumbNode: SpectrumSliderThumb,
+    wavelengthReadout: NumberDisplay,
+    wavelengthTrackNode: LabeledSpectrumSliderTrack,
+    readoutAboveSliderSpacing: number
+  ): void {
+    if ( rowNode.bounds.isValid() && wavelengthThumbNode.bounds.isValid() && wavelengthReadout.bounds.isValid() ) {
+      const thumbCenterLocal = wavelengthThumbNode.localBounds.center;
+      const thumbCenterGlobal = wavelengthThumbNode.localToGlobalPoint( thumbCenterLocal );
+      const thumbCenterInRow = rowNode.globalToLocalPoint( thumbCenterGlobal );
+
+      const valueToPosition = wavelengthTrackNode.valueToPositionProperty.value;
+      const wavelengthRange = photonSource.wavelengthProperty.range;
+      const trackLeftInRow = rowNode.globalToLocalPoint(
+        wavelengthTrackNode.localToGlobalPoint( new Vector2( valueToPosition.evaluate( wavelengthRange.min ), 0 ) )
+      ).x;
+      const trackRightInRow = rowNode.globalToLocalPoint(
+        wavelengthTrackNode.localToGlobalPoint( new Vector2( valueToPosition.evaluate( wavelengthRange.max ), 0 ) )
+      ).x;
+
+      const halfReadoutWidth = wavelengthReadout.width / 2;
+      const minCenterX = trackLeftInRow + halfReadoutWidth;
+      const maxCenterX = trackRightInRow - halfReadoutWidth;
+
+      let centerX = thumbCenterInRow.x;
+      if ( minCenterX <= maxCenterX ) {
+        centerX = clamp( centerX, minCenterX, maxCenterX );
+      }
+      else {
+
+        // Readout wider than the track interior: keep it centered on the bar.
+        centerX = ( trackLeftInRow + trackRightInRow ) / 2;
+      }
+
+      wavelengthReadout.centerX = centerX;
+      wavelengthReadout.bottom = wavelengthSlider.top - readoutAboveSliderSpacing;
+    }
+  }
+}
