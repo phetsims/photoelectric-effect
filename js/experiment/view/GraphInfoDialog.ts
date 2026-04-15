917390a0134f9726832365db96f6a984d2f9751d
Jesse <jesse.greenberg@colorado.edu>
2026-04-15 15:08:39 -0400

    sync-to-poly: initial commit for the intensity and wavelength control, see #https://github.com/phetsims/photoelectric-effect/issues/25

--- js/experiment/view/GraphInfoDialog.ts
+++ js/experiment/view/GraphInfoDialog.ts
@@ -7,37 +7,31 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
 import Dialog from '../../../../sun/js/Dialog.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
+import PhotoelectricEffectConstants from '../../common/PhotoelectricEffectConstants.js';
 import PhotoelectricEffectFluent from '../../PhotoelectricEffectFluent.js';
 
-// Determined empirically for readable line lengths in the dialog body.
-const MAX_CONTENT_WIDTH = 480;
-
-const DIALOG_TITLE_FONT = new PhetFont( { size: 18, weight: 'bold' } );
-const DIALOG_CONTENT_FONT = new PhetFont( 14 );
-
-export default class ExperimentGraphInfoDialog extends Dialog {
+export default class GraphInfoDialog extends Dialog {
 
   public constructor( tandem: Tandem ) {
 
     const titleText = new Text( PhotoelectricEffectFluent.experiment.graph.infoDialogTitleStringProperty, {
-      font: DIALOG_TITLE_FONT,
-      maxWidth: MAX_CONTENT_WIDTH
+      font: PhotoelectricEffectConstants.DIALOG_TITLE_FONT,
+      maxWidth: PhotoelectricEffectConstants.DIALOG_MAX_CONTENT_WIDTH
     } );
 
     const placeholderText = new Text( PhotoelectricEffectFluent.experiment.graph.infoDialogPlaceholderStringProperty, {
-      font: DIALOG_CONTENT_FONT,
-      maxWidth: MAX_CONTENT_WIDTH
+      font: PhotoelectricEffectConstants.DIALOG_CONTENT_FONT,
+      maxWidth: PhotoelectricEffectConstants.DIALOG_MAX_CONTENT_WIDTH
     } );
 
     super( placeholderText, {
       title: titleText,
-      xSpacing: 30,
-      cornerRadius: 10,
-      ySpacing: 30,
+      xSpacing: PhotoelectricEffectConstants.DIALOG_SPACING,
+      cornerRadius: PhotoelectricEffectConstants.DIALOG_CORNER_RADIUS,
+      ySpacing: PhotoelectricEffectConstants.DIALOG_SPACING,
       isDisposable: false,
       tandem: tandem,
       phetioReadOnly: true
