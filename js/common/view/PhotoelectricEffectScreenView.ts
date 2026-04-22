3dcf1d0fb54ae4499caf164a255dfc43a3b87b3d
Jesse <jesse.greenberg@colorado.edu>
2026-04-22 13:09:48 -0400

    sync-to-poly: add todo for https://github.com/phetsims/photoelectric-effect/issues/38
    Mono-SHA: 40fdc8e26a6194fbeecc1934659fce5a1ecfb8c2

--- js/common/view/PhotoelectricEffectScreenView.ts
+++ js/common/view/PhotoelectricEffectScreenView.ts
@@ -51,6 +51,8 @@ export default class PhotoelectricEffectScreenView extends ScreenView {
 
     // TODO: Toggle comboBox item visibility based on PhotoelectricEffectPreferences.mysteryMaterialEnabledProperty, see
     // https://github.com/phetsims/photoelectric-effect/issues/5
+    // TODO: Placement and visibility of this control. Will be best to do that once other changes are merged
+    //  into main. See 
     const materialsComboBox = new MaterialsComboBox( model.target.materialProperty, model.target.materials, this, {
       tandem: options.tandem.createTandem( 'materialsComboBox' )
     } );
