f9710e28bd9ca15de620a4b8708f300a0430ba24
Jesse <jesse.greenberg@colorado.edu>
2026-05-05 12:26:34 -0400

    sync-to-poly: address todos in IntroScreen, see https://github.com/phetsims/photoelectric-effect/issues/63
    Mono-SHA: 7c0e1b3b259a987ef81723caf2ad7b55d985cf4b

--- js/intro/IntroScreen.ts
+++ js/intro/IntroScreen.ts
@@ -1,24 +1,21 @@
 // Copyright 2026, University of Colorado Boulder
 
 /**
- * TODO Describe this class and its responsibilities.
+ * Screen class for the Intro screen in the photoelectric effect simulation.
  *
  * @author Marla Schulz (PhET Interactive Simulations)
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
 import Screen, { ScreenOptions } from '../../../joist/js/Screen.js';
-import optionize from '../../../phet-core/js/optionize.js';
+import optionize, { EmptySelfOptions } from '../../../phet-core/js/optionize.js';
 import Material from '../common/model/Material.js';
 import PhotoelectricEffectColors from '../common/PhotoelectricEffectColors.js';
 import PhotoelectricEffectFluent from '../PhotoelectricEffectFluent.js';
 import IntroModel from './model/IntroModel.js';
 import IntroScreenView from './view/IntroScreenView.js';
 
-type SelfOptions = {
-  //TODO add options that are specific to PhotoelectricEffectScreen here
-};
-
+type SelfOptions = EmptySelfOptions;
 type PhotoelectricEffectScreenOptions = SelfOptions & ScreenOptions;
 
 export default class IntroScreen extends Screen<IntroModel, IntroScreenView> {
@@ -27,10 +24,6 @@ export default class IntroScreen extends Screen<IntroModel, IntroScreenView> {
 
     const options = optionize<PhotoelectricEffectScreenOptions, SelfOptions, ScreenOptions>()( {
       name: PhotoelectricEffectFluent.screen.introStringProperty,
-
-      //TODO add default values for optional SelfOptions here
-
-      //TODO add default values for optional ScreenOptions here
       backgroundColorProperty: PhotoelectricEffectColors.screenBackgroundColorProperty
     }, providedOptions );
 
