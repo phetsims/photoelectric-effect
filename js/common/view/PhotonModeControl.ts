cb8d4156720e769565d8ff20951c9978ed9543a2
Jesse <jesse.greenberg@colorado.edu>
2026-05-05 11:37:45 -0400

    sync-to-poly: swap values on the photon mode control, see https://github.com/phetsims/photoelectric-effect/issues/58
    Mono-SHA: ea50915461063450f5e1ed407bb8cfd924f0eba1

--- js/common/view/PhotonModeControl.ts
+++ js/common/view/PhotonModeControl.ts
@@ -26,8 +26,8 @@ export default class PhotonModeControl extends PreferencesControl {
     //  out in this sim? This combineOPtions appears twice currently. May not be worth it.
     const toggleSwitch = new ToggleSwitch(
       PhotoelectricEffectPreferences.photonModeProperty,
-      'intensity',
-      'count', combineOptions<ToggleSwitchOptions>(
+      'count',
+      'intensity', combineOptions<ToggleSwitchOptions>(
         {},
         {
           tandem: providedOptions.tandem.createTandem( 'toggleSwitch' ),
