980f6cd4c358bbcd2b85f21b71ff20bcd801e5cf
Jesse <jesse.greenberg@colorado.edu>
2026-06-05 12:53:38 -0400

    sync-to-poly: typo fixes, see https://github.com/phetsims/totality/pull/172
    Mono-SHA: 615c4c8129737236ae2cb13a051d44928117808f

--- js/common/model/Target.ts
+++ js/common/model/Target.ts
@@ -138,7 +138,7 @@ export default class Target {
       // 1/20 is an empirically determined threshold confirmed chosen by design team. Critically, this only
       // affects the electrons that are created for the experiment view. It does not affect the current calculation.
       // It is intended to match the behavior of "Highest Energy Only" in the Java version. Electrons are emitted
-      // with highest possible energy, but we only about 5% of them as this represents a small sample of the
+      // with the highest possible energy, but we only show about 5% of them as this represents a small sample of the
       // possible energies.
       energyAfterCollision = dotRandom.nextDouble() < 1 / 20 ?
                              photonEnergy - workFunction :
