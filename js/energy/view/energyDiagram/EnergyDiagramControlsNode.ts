54fc0f7b29675223f709299af02e2f09483a066c
Jesse <jesse.greenberg@colorado.edu>
2026-06-29 11:46:33 -0400

    sync-to-poly: add an arrow icon and some TODOs, see https://github.com/phetsims/photoelectric-effect/issues/153
    Mono-SHA: cdf38948fbb6af325e7931cfb12768623709f128

--- js/energy/view/energyDiagram/EnergyDiagramControlsNode.ts
+++ js/energy/view/energyDiagram/EnergyDiagramControlsNode.ts
@@ -10,12 +10,15 @@ import DerivedStringProperty from '../../../../../axon/js/DerivedStringProperty.
 import Property from '../../../../../axon/js/Property.js';
 import optionize, { EmptySelfOptions } from '../../../../../phet-core/js/optionize.js';
 import PickRequired from '../../../../../phet-core/js/types/PickRequired.js';
+import ArrowNode from '../../../../../scenery-phet/js/ArrowNode.js';
 import MathSymbols from '../../../../../scenery-phet/js/MathSymbols.js';
+import HBox from '../../../../../scenery/js/layout/nodes/HBox.js';
 import VBox, { type VBoxOptions } from '../../../../../scenery/js/layout/nodes/VBox.js';
 import RichText from '../../../../../scenery/js/nodes/RichText.js';
 import Text from '../../../../../scenery/js/nodes/Text.js';
 import Checkbox from '../../../../../sun/js/Checkbox.js';
 import Tandem from '../../../../../tandem/js/Tandem.js';
+import PhotoelectricEffectColors from '../../../common/PhotoelectricEffectColors.js';
 import PhotoelectricEffectConstants from '../../../common/PhotoelectricEffectConstants.js';
 import PhotoelectricEffectFluent from '../../../PhotoelectricEffectFluent.js';
 
@@ -26,8 +29,15 @@ type EnergyDiagramControlsNodeOptions = SelfOptions & VBoxOptions & PickRequired
 const CHECKBOX_SPACING = 10;
 
 // Checkbox layout.
-const CHECKBOX_LABEL_MAX_WIDTH = 110;
+const CHECKBOX_LABEL_MAX_WIDTH = 130;
 const CHECKBOX_BOX_WIDTH = 17;
+const PHOTON_ARROW_ICON_LENGTH = 18;
+const PHOTON_ARROW_ICON_SPACING = 6;
+const PHOTON_ARROW_ICON_TAIL_WIDTH = 1.25;
+const PHOTON_ARROW_ICON_HEAD_WIDTH = 8;
+const PHOTON_ARROW_ICON_HEAD_HEIGHT = 8;
+const PHOTON_ARROW_CHECKBOX_LABEL_MAX_WIDTH = CHECKBOX_LABEL_MAX_WIDTH - PHOTON_ARROW_ICON_HEAD_WIDTH -
+                                              PHOTON_ARROW_ICON_SPACING;
 
 /**
  * Stacks diagram-specific controls so they can be composed with shared graph controls in the accordion.
@@ -78,9 +88,25 @@ export default class EnergyDiagramControlsNode extends VBox {
 
         new Checkbox(
           photonArrowsVisibleProperty,
-          new Text( PhotoelectricEffectFluent.energy.graph.diagramControls.photonArrowStringProperty, {
-            font: PhotoelectricEffectConstants.CONTENT_FONT,
-            maxWidth: CHECKBOX_LABEL_MAX_WIDTH
+          new HBox( {
+            spacing: PHOTON_ARROW_ICON_SPACING,
+            align: 'center',
+            children: [
+              new Text( PhotoelectricEffectFluent.energy.graph.diagramControls.photonArrowStringProperty, {
+                font: PhotoelectricEffectConstants.CONTENT_FONT,
+                maxWidth: PHOTON_ARROW_CHECKBOX_LABEL_MAX_WIDTH
+              } ),
+
+              // Icon matching the photon arrows drawn in the energy diagram.
+              // TODO: Factor out into some kind of icon factory, see https://github.com/phetsims/photoelectric-effect/issues/153
+              new ArrowNode( 0, PHOTON_ARROW_ICON_LENGTH, 0, 0, {
+                fill: PhotoelectricEffectColors.photonArrowEnergyDiagramColorProperty,
+                stroke: PhotoelectricEffectColors.photonArrowEnergyDiagramColorProperty,
+                tailWidth: PHOTON_ARROW_ICON_TAIL_WIDTH,
+                headWidth: PHOTON_ARROW_ICON_HEAD_WIDTH,
+                headHeight: PHOTON_ARROW_ICON_HEAD_HEIGHT
+              } )
+            ]
           } ), {
             boxWidth: CHECKBOX_BOX_WIDTH,
             spacing: 4,
--- js/energy/view/energyDiagram/EnergyDiagramSampleMarkerNode.ts
+++ js/energy/view/energyDiagram/EnergyDiagramSampleMarkerNode.ts
@@ -82,7 +82,7 @@ export default class EnergyDiagramSampleMarkerNode extends Node {
       headHeight: PHOTON_ARROW_HEAD_HEIGHT
     } );
 
-    // This is meant to be a dashed arrow, see https://github.com/phetsims/scenery-phet/issues/533 and
+    // TODO This is meant to be a dashed arrow, see https://github.com/phetsims/scenery-phet/issues/533 and
     // custom DashedArrowNode in sims.
     const failedPhotonArrowNode = new ArrowNode( sampleCenterX, sampleInitialY, sampleCenterX, sampleInitialY, {
       fill: null,
